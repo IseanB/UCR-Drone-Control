e3bc057cb6450dcbae713c9c771ebbf14273fec9
IseanB <Isean4B@gmail.com>
2022-08-24 14:41:38 -0700

    Removed excess functions

--- helper/src/computations.cpp
+++ helper/src/computations.cpp
@@ -16,14 +16,6 @@ bool isFlat(const geometry_msgs::Pose& this_pos, float maxTilt){
     return (abs(this_pos.orientation.x) <= abs(maxTilt) && abs(this_pos.orientation.y) <= abs(maxTilt));
 }
 
-bool reachedLocation(const geometry_msgs::PoseStamped& this_pos, const geometry_msgs::PoseStamped& desired_pos, float accuracyDistance){
-    return( 
-        pow( 
-            pow(this_pos.pose.position.x - desired_pos.pose.position.x, 2) + 
-            pow(this_pos.pose.position.y - desired_pos.pose.position.y, 2) + 
-            pow(this_pos.pose.position.z - desired_pos.pose.position.z, 2), .5) <= abs(accuracyDistance) );
-}
-
 bool reachedLocation(const geometry_msgs::Pose& this_pos, const geometry_msgs::PoseStamped& desired_pos, float accuracyDistance){
     return( 
         pow( 
@@ -32,13 +24,6 @@ bool reachedLocation(const geometry_msgs::Pose& this_pos, const geometry_msgs::P
             pow(this_pos.position.z - desired_pos.pose.position.z, 2), .5) <= abs(accuracyDistance) );
 }
 
-bool reachedLocation(const geometry_msgs::PoseStamped& this_pos, const mavros_msgs::PositionTarget& desired_pos, float accuracyDistance){
-    return( 
-        pow( 
-            pow(this_pos.pose.position.x - desired_pos.position.x, 2) + 
-            pow(this_pos.pose.position.y - desired_pos.position.y, 2) + 
-            pow(this_pos.pose.position.z - desired_pos.position.z, 2), .5) <= abs(accuracyDistance) );
-}
 
 bool reachedLocation(const geometry_msgs::Pose& this_pos, const mavros_msgs::PositionTarget& desired_pos, float accuracyDistance){
     return( 
